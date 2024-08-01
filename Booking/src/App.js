02022e16c9d2ebf34ea8655e6fb0c1451905a4be
Rahul <rahulkumawat50665@gmail.com>
2024-08-01 05:15:01 -0400

    Added starting project

---
+++ Booking/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Home page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
